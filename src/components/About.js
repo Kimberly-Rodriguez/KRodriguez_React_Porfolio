4e11ec11f178b1d5f9c2abb731ab0ce4e2457e89
kimberly rodriguez <krodriguez.ucla@gmail.com>
2022-12-12 09:35:13 -0800

    update about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../App.css";
 
-
 export default function About() {
   return (
     <div className="container-fluid p-0">
@@ -10,35 +9,47 @@ export default function About() {
         <div className="resume-section-content">
           <h2 className="mb-0">Kimberly Rodriguez</h2>
           <div>
-            <h3 className="mb-0"><span className="text-primary">Software Engineer</span></h3>
+            <h3 className="mb-0">
+              <span className="text-primary">Software Engineer</span>
+            </h3>
           </div>
           <p className="lead mb-5"></p>
           <p>
-            I am a software engineer fluent in programming languages and databases like JavaScript, React, and MySQL.
+            I am a software engineer fluent in programming languages and
+            databases like JavaScript, React, and MySQL.
           </p>
-          <p>
+          {/* <p>
             Prior to this career shift, I was a civic innovator with more than
             seven years of experience in the highest levels of local government.
           </p>
           <p>
             I led and supported initiatives around open data, economic
             development, housing accessibility, and youth leadership.
-          </p>
+          </p> */}
           <p>
-            For additonal details about my work please view my portfolio and experience section. 
+            For additonal details about my work please view my portfolio and
+            experience section.
           </p>
           <p>Talk to you soon!</p>
           <br />
           <br />
           <div className="social-icons">
-            <a className="social-icon" href="https://www.bit.ly/kmbrly-rdrgz"><i className="fab fa-linkedin-in"></i></a>
-            <a className="social-icon" href="https://www.bit.ly/kr-github"><i className="fab fa-github"></i></a>
-            <a className="social-icon" href="https://twitter.com/KimRodriguezLA"><i className="fab fa-twitter"></i>
+            <a className="social-icon" href="https://www.bit.ly/kmbrly-rdrgz">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+            <a className="social-icon" href="https://www.bit.ly/kr-github">
+              <i className="fab fa-github"></i>
+            </a>
+            <a
+              className="social-icon"
+              href="https://twitter.com/KimRodriguezLA"
+            >
+              <i className="fab fa-twitter"></i>
             </a>
           </div>
         </div>
       </section>
-      <hr className="m-0"/>
+      <hr className="m-0" />
     </div>
   );
 }
